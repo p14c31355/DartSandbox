cf4d7a477e12bc32cdc1bbd50ef983f8a0c5082d
Placeless <thinkoverheat@gmail.com>
2026-04-08 00:12:57 +0900

    Color change test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.blue),
       ),
       home: const MyHomePage(title: 'Flutter Demo'),
     );
