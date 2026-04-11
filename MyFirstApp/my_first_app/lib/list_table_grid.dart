6426aef981070232ae01b084598437ae9523fc41
Placeless <thinkoverheat@gmail.com>
2026-04-11 10:27:52 +0900

    chore: quick fix

--- MyFirstApp/my_first_app/lib/list_table_grid.dart
+++ MyFirstApp/my_first_app/lib/list_table_grid.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ListTableGrid extends StatefulWidget {
+  const ListTableGrid({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _ListTableState createState() => _ListTableState();
 }
 
